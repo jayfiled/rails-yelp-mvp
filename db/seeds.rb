2fdc9f917a341fd6a89e8c77d041a04b270c642a
Joel James <jayfiled@gmail.com>
2019-05-22 15:02:49 +1000

    set up models with validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database'
+Restaurant.destroy_all
+
+puts 'Adding seeds...'
+
+restaurant_attributes = [
+  {
+    name: 'Bikkuri Donkey',
+    address: '123 Higashi-ku, Sendai',
+    phone_number: '55-1203-1222',
+    category: 'japanese'
+  },
+  {
+    name: 'Lokkate',
+    address: '122 Higashi-ku, Tokyo',
+    phone_number: '55-3746-1255',
+    category: 'belgian'
+  },
+  {
+    name: 'Mos Burger',
+    address: '121 Higashi-ku, Osaka',
+    phone_number: '55-1223-1344',
+    category: 'french'
+  },
+  {
+    name: 'Lawson',
+    address: '125 Higashi-ku, Sapporo',
+    phone_number: '55-2736-1822',
+    category: 'italian'
+  }
+]
+
+Restaurant.create!(restaurant_attributes)
+
+puts 'Finished!'
